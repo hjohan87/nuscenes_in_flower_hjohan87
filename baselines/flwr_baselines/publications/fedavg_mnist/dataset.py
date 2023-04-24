cee72e5b9c41a923e34813e58b28646f8128868a
dolander1 <dolander@volvocars.com>
2023-04-24 09:53:43 +0000

    moved around results + testing shorter runs

--- baselines/flwr_baselines/publications/fedavg_mnist/dataset.py
+++ baselines/flwr_baselines/publications/fedavg_mnist/dataset.py
@@ -232,11 +232,11 @@ def _loadData():
     ################################################################################################################################################
 
     # For testing
-    train_short_size = 2000
+    train_short_size = 500
     short_train_img_tensor_list = train_img_tensor_list[:train_short_size]
     short_train_agent_state_vector_list = train_agent_state_vector_list[:train_short_size]
     short_train_future_xy_local_list = train_future_xy_local_list[:train_short_size]
-    val_short_size = 400
+    val_short_size = 100
     short_val_img_tensor_list = val_img_tensor_list[:val_short_size]
     short_val_agent_state_vector_list = val_agent_state_vector_list[:val_short_size]
     short_val_future_xy_local_list = val_future_xy_local_list[:val_short_size]
@@ -260,7 +260,7 @@ def _loadData():
 
 
     # Variables
-    batch_size = 16
+    batch_size = 10
     shuffle = True # Set to True if you want to shuffle the data in the dataloader
     # num_modes = 64 # 2206, 415, 64 (match with eps_traj_set)
     # eps_traj_set = 8 # 2, 4, 8 (match with num_modes)
--- baselines/flwr_baselines/publications/fedavg_mnist/model.py
+++ baselines/flwr_baselines/publications/fedavg_mnist/model.py
@@ -56,7 +56,7 @@ class Net(nn.Module): # EXTRA TMP
                          for in_dim, out_dim in zip(n_hidden_layers[:-1], n_hidden_layers[1:])]
 
         self.head = nn.ModuleList(linear_layers)
-#         self.relu = nn.ReLU() # Daniel
+        self.relu = nn.ReLU() # Daniel
 
     def forward(self, image_tensor: torch.Tensor,
                 agent_state_vector: torch.Tensor) -> torch.Tensor:
@@ -71,8 +71,8 @@ class Net(nn.Module): # EXTRA TMP
         logits = torch.cat([backbone_features, agent_state_vector], dim=1)
 
         for linear in self.head:
-#             logits = self.relu(linear(logits)) # Daniel
-            logits = linear(logits)
+            logits = self.relu(linear(logits)) # Daniel
+#             logits = linear(logits)
 
 
         return logits
@@ -294,6 +294,7 @@ def test(
             image_tensor = image_tensor.to(device)
             agent_state_vector = agent_state_vector.to(device)
             ground_truth_trajectory = ground_truth_trajectory.to(device)
+            
             logits = net(image_tensor, agent_state_vector)
             loss += criterion(logits, ground_truth_trajectory)#.item()
 #             print("lattice shape:", lattice.shape)
